a6f569fe837f2ab0af773cc47e2f1fbc9f0943b5
Evan Hahn <evan.hahn@airtable.com>
2019-04-29 15:05:12 -0500

    Test record creation and deletion with callbacks instead of promises (#94)
    Per [a comment on another PR][1], this tests record creation and
    deletion with callbacks instead of promises.
    
    This only changes tests, which pass with `npm test`.
    
    [1]: https://github.com/Airtable/airtable.js/pull/92/files#r279134648

--- test/create.test.js
+++ test/create.test.js
@@ -32,6 +32,22 @@ describe('record creation', function () {
       });
   });
 
+  it('can create one record and call a callback', function (done) {
+    airtable
+      .base('app123')
+      .table('Table')
+      .create({
+        foo: 'boo',
+        bar: 'yar',
+      }, function (err, createdRecord) {
+        expect(err).toBeNull();
+        expect(createdRecord.id).toBe('rec0');
+        expect(createdRecord.get('foo')).toBe('boo');
+        expect(createdRecord.get('bar')).toBe('yar');
+        done();
+      });
+  });
+
   it('can add the "typecast" parameter when creating one record', function () {
     return airtable
       .base('app123')
@@ -72,6 +88,24 @@ describe('record creation', function () {
         expect(createdRecords[0].get('foo')).toBe('boo');
         expect(createdRecords[1].id).toBe('rec1');
         expect(createdRecords[1].get('bar')).toBe('yar');
+      });
+  });
+
+  it('can create two records and call a callback', function (done) {
+    airtable
+      .base('app123')
+      .table('Table')
+      .create([
+        {foo: 'boo'},
+        {bar: 'yar'},
+      ], function (err, createdRecords) {
+        expect(err).toBeNull();
+        expect(createdRecords).toHaveLength(2);
+        expect(createdRecords[0].id).toBe('rec0');
+        expect(createdRecords[0].get('foo')).toBe('boo');
+        expect(createdRecords[1].id).toBe('rec1');
+        expect(createdRecords[1].get('bar')).toBe('yar');
+        done();
       });
   });
 
