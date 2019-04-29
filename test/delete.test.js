5c2a33314065558639252fb995d28db60d04c960
Evan Hahn <evan.hahn@airtable.com>
2019-04-29 14:16:28 -0500

    Test record creation and deletion with callbacks instead of promises
    Per [a comment on another PR][1], this tests record creation and
    deletion with callbacks instead of promises.
    
    This only changes tests, which pass with `npm test`.
    
    [1]: https://github.com/Airtable/airtable.js/pull/92/files#r279134648

--- test/delete.test.js
+++ test/delete.test.js
@@ -27,6 +27,17 @@ describe('record deletion', function () {
       });
   });
 
+  it('can delete one record and call a callback', function (done) {
+    airtable
+      .base('app123')
+      .table('Table')
+      .destroy('rec123', function (err, deletedRecord) {
+        expect(err).toBeNull();
+        expect(deletedRecord.id).toBe('rec123');
+        done();
+      });
+  });
+
   it('can delete multiple records', function () {
     return airtable
       .base('app123')
@@ -36,6 +47,20 @@ describe('record deletion', function () {
         expect(deletedRecords).toHaveLength(2);
         expect(deletedRecords[0].id).toBe('rec123');
         expect(deletedRecords[1].id).toBe('rec456');
+      });
+  });
+
+  it('can delete multiple records and call a callback', function (done) {
+    airtable
+      .base('app123')
+      .table('Table')
+      .destroy(['rec123', 'rec456'], function (err, deletedRecords) {
+        expect(err).toBeNull();
+        expect(deletedRecords).toHaveLength(2);
+        expect(deletedRecords).toHaveLength(2);
+        expect(deletedRecords[0].id).toBe('rec123');
+        expect(deletedRecords[1].id).toBe('rec456');
+        done();
       });
   });
 });
