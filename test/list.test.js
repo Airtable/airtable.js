a27c1340e4a2d15913bc3f738fd48fbd29249e6a
Rebecca Meritz <rebecca@meritz.com>
2020-06-04 10:45:28 -0700

    Add tests for Table#forEach

--- test/list.test.js
+++ test/list.test.js
@@ -100,4 +100,130 @@ describe('list records', function() {
                 done();
             });
     });
+
+    it('iterates through each record with opts', function(done) {
+        var json = {
+            records: [
+                {
+                    id: 'recordA',
+                    fields: {Name: 'Rebecca'},
+                    createdTime: '2020-04-20T16:20:00.000Z',
+                },
+            ],
+        };
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table/?limit=100&opts=arepassedalong');
+            res.json(json);
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .forEach(
+                {opts: 'arepassedalong'},
+                function(record) {
+                    expect(record.getId()).toBe('recordA');
+                    expect(record.get('Name')).toBe('Rebecca');
+                },
+                function() {
+                    done();
+                }
+            );
+    });
+
+    it('iterates through each record without opts', function(done) {
+        var json = {
+            records: [
+                {
+                    id: 'recordA',
+                    fields: {Name: 'Rebecca'},
+                    createdTime: '2020-04-20T16:20:00.000Z',
+                },
+            ],
+        };
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table/?limit=100');
+            res.json(json);
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .forEach(
+                function(record) {
+                    expect(record.getId()).toBe('recordA');
+                    expect(record.get('Name')).toBe('Rebecca');
+                },
+                function() {
+                    done();
+                }
+            );
+    });
+
+    it('can throw an error if forEach fails', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            res.status(402).json({
+                error: {message: 'foo bar'},
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .forEach(
+                {opts: 'arepassedalong'},
+                function() {},
+                function(err) {
+                    expect(err).not.toBeNull();
+                    done();
+                }
+            );
+    });
+
+    it('iterates through each record and handles pagination', function(done) {
+        var json = {
+            records: [
+                {
+                    id: 'recordA',
+                    fields: {Name: 'Rebecca'},
+                    createdTime: '2020-04-20T16:20:00.000Z',
+                },
+            ],
+            offset: 'offset123',
+        };
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table/?limit=100&opts=arepassedalong');
+            res.json(json);
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .forEach(
+                {opts: 'arepassedalong'},
+                function(record) {
+                    expect(record.getId()).toBe('recordA');
+                    expect(record.get('Name')).toBe('Rebecca');
+
+                    // Reset API response to handle the paginated call
+                    testExpressApp.set('handler override', function(req, res) {
+                        expect(req.method).toBe('GET');
+                        expect(req.url).toBe(
+                            '/v0/app123/Table/?limit=100&offset=offset123&opts=arepassedalong'
+                        );
+                        delete json.offset;
+                        res.json(json);
+                    });
+                },
+                function() {
+                    done();
+                }
+            );
+    });
 });
