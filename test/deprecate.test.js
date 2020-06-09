17fe5a2c6fea1266f0f2d092a4f9c81615db4cbe
Rebecca Meritz <rebecca@meritz.com>
2020-06-09 14:41:14 -0700

    Add deprecation.js tests
    Ensure it applies the function and warns only one time

---
+++ test/deprecate.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var deprecate = require('../lib/deprecate');
+
+describe('Airtable', function() {
+    var x;
+    var addOne;
+    var plusPlus;
+    var spy;
+
+    beforeEach(function() {
+        addOne = function(n) {
+            x = n + 1;
+        };
+        plusPlus = deprecate(addOne, 'addOne', 'addOne has been deprecated');
+        spy = jest.spyOn(console, 'warn').mockImplementation();
+    });
+
+    afterEach(function() {
+        spy.mockRestore();
+    });
+
+    it('applys the function and warns once', function() {
+        plusPlus(1);
+        expect(x).toBe(2);
+        expect(console.warn).toHaveBeenCalledTimes(1);
+        plusPlus(1);
+        expect(console.warn).toHaveBeenCalledTimes(1);
+    });
+});
