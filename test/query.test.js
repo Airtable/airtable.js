0b5e0595cef85e289c6a46679153256e8b86c871
Rebecca Meritz <rebecca@meritz.com>
2020-06-17 13:17:58 -0700

    Remove param validation from Query constructor
    It is not used because validation id done with Query.validateParams in
    `lib/table.js`.

--- test/query.test.js
+++ test/query.test.js
@@ -3,18 +3,6 @@
 var Query = require('../lib/query');
 
 describe('Query', function() {
-    it('throws an error with invalid parameter value', function() {
-        return expect(() => {
-            new Query('table', {maxRecords: '5'});
-        }).toThrow(/Invalid parameter/);
-    });
-
-    it('throws an error with invalid parameter keys', function() {
-        return expect(() => {
-            new Query('table', {unknownKey: '5'});
-        }).toThrow(/Invalid parameter/);
-    });
-
     it('throws an error on validating invalid params', function() {
         return expect(() => {
             Query.validateParams('?param!=object');
