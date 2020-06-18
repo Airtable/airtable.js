66b9e5decd3a3db9aaf29e74d3b8883939663f3a
Rebecca Meritz <rebecca@meritz.com>
2020-06-17 18:47:45 -0700

    Add test coverage for `lib/run_action.js` (#187)
    * Add test of retry if hitting rate limit
    
    * Add test for custom user agent header if browser
    
    * Correctly cleanup after global mock
    
    * Remove unneeded dones

--- test/find.test.js
+++ test/find.test.js
@@ -16,6 +16,7 @@ describe('record retrival', function() {
     });
 
     afterAll(function() {
+        delete global.window;
         return teardownAsync();
     });
 
@@ -25,6 +26,7 @@ describe('record retrival', function() {
         testExpressApp.set('handler override', function(req, res) {
             expect(req.method).toBe('GET');
             expect(req.url).toBe('/v0/app123/Table/record1?');
+            expect(req.get('user-agent')).toMatch(/Airtable.js/);
             res.json({
                 id: req.params.recordId,
                 fields: {Name: 'Rebecca'},
@@ -42,7 +44,32 @@ describe('record retrival', function() {
             });
     });
 
-    it('can handle an error', function(done) {
+    it('set the correct headers in a browser', function() {
+        global.window = {};
+        var recordId = 'record1';
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table/record1?');
+            expect(req.get('x-airtable-user-agent')).toMatch(/Airtable.js/);
+            res.json({
+                id: req.params.recordId,
+                fields: {Name: 'Rebecca'},
+                createdTime: '2020-04-20T16:20:00.000Z',
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .find(recordId)
+            .then(function(foundRecord) {
+                expect(foundRecord.id).toBe(recordId);
+                expect(foundRecord.get('Name')).toBe('Rebecca');
+            });
+    });
+
+    it('can handle an error', function() {
         testExpressApp.set('handler override', function(req, res) {
             res.status(402).json({
                 error: {message: 'foo bar'},
@@ -60,8 +87,38 @@ describe('record retrival', function() {
                 function(err) {
                     expect(err.statusCode).toBe(402);
                     expect(err.message).toBe('foo bar');
-                    done();
                 }
             );
+    });
+
+    it('can find after a retry if rate limited', function() {
+        var apiCallCount = 0;
+        var recordId = 'record1';
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table/record1?');
+            if (apiCallCount === 0) {
+                res.status(429).json({
+                    error: {message: 'Over rate limit'},
+                });
+            } else if (apiCallCount === 1) {
+                res.json({
+                    id: req.params.recordId,
+                    fields: {Name: 'Rebecca'},
+                    createdTime: '2020-04-20T16:20:00.000Z',
+                });
+            }
+            apiCallCount++;
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .find('record1')
+            .then(function(foundRecord) {
+                expect(foundRecord.id).toBe(recordId);
+                expect(foundRecord.get('Name')).toBe('Rebecca');
+            });
     });
 });
