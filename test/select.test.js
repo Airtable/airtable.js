c8b9ea261d69d760c2522e45c8857c0a02f8ffc9
Rebecca Meritz <rebecca@meritz.com>
2020-06-22 20:15:09 -0700

    Complete `lib/query.js` tests (#186)
    * Add query.js tests
    
    * Istanbul ignore branch
    
    The else path will never be taken because callbackToPromise which wraps
    this function call will ensure there is always a `done` function.
    
    * Remove unused if
    
    callbackToPromise ensures there will always be a `done` function.
    
    * Remove test for unused error code path
    
    There is error checking for a plain object at `lib/table.js:63` and
    `Query` is meant to be a private interface so this code path will never
    be hit.
    
    * Remove param validation from Query constructor
    
    It is not used because validation id done with Query.validateParams in
    `lib/table.js`.
    
    * Remove checking that Query is a plain object
    
    This checking is done in `lib/table.js` so it isn't needed here.
    
    * Remove test for cellFormat
    
    Move use of cellFormat into select test so that query.test.js can be
    remove.

--- test/select.test.js
+++ test/select.test.js
@@ -49,11 +49,260 @@ describe('record selection', function() {
             });
     });
 
+    it('selects records more than one record', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                    {
+                        id: 'recordB',
+                        fields: {Name: 'Drew'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .eachPage(function page(records) {
+                expect(records.length).toBe(2);
+                expect(records[0].id).toBe('recordA');
+                expect(records[0].get('Name')).toBe('Rebecca');
+                expect(records[1].id).toBe('recordB');
+                expect(records[1].get('Name')).toBe('Drew');
+                done();
+            });
+    });
+
+    it('selects all records', function(done) {
+        var iterationCounter = 0;
+
+        testExpressApp.set('handler override', function(req, res) {
+            if (iterationCounter === 0) {
+                expect(req.method).toBe('GET');
+                expect(req.url).toBe('/v0/app123/Table?');
+                res.json({
+                    records: [
+                        {
+                            id: 'recordA',
+                            fields: {Name: 'Rebecca'},
+                            createdTime: '2020-04-20T16:20:00.000Z',
+                        },
+                    ],
+                    offset: 'offsetABC',
+                });
+            } else if (iterationCounter === 1) {
+                expect(req.method).toBe('GET');
+                expect(req.url).toBe('/v0/app123/Table?offset=offsetABC');
+                res.json({
+                    records: [
+                        {
+                            id: 'recordB',
+                            fields: {Name: 'Drew'},
+                            createdTime: '2020-04-20T16:20:00.000Z',
+                        },
+                    ],
+                });
+            }
+            iterationCounter++;
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .all(function(err, records) {
+                expect(err).toBeNull();
+                expect(records.length).toBe(2);
+                expect(records[0].id).toBe('recordA');
+                expect(records[0].get('Name')).toBe('Rebecca');
+                expect(records[1].id).toBe('recordB');
+                expect(records[1].get('Name')).toBe('Drew');
+                expect(iterationCounter).toBe(2);
+                done();
+            });
+    });
+
+    it('handles errors when selecting  all records', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            res.status(402).json({
+                error: {message: 'foo bar'},
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .all(function(err, result) {
+                expect(err.statusCode).toBe(402);
+                expect(err.message).toBe('foo bar');
+                expect(result).toBeNull();
+                done();
+            });
+    });
+
+    it('all errors on the first invalid parameter', function() {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return expect(
+            airtable
+                .base('app123')
+                .table('Table')
+                .select()
+                .all('monkeys')
+        ).rejects.toThrow(/must be a function/);
+    });
+
+    it('selects records and paginates the selection without a done function', function(done) {
+        var iterationCounter = 0;
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .eachPage(function page(records, fetchNextPage) {
+                if (iterationCounter === 0) {
+                    expect(records.length).toBe(1);
+                    records.forEach(function(record) {
+                        expect(record.id).toBe('recordA');
+                        expect(record.get('Name')).toBe('Rebecca');
+                    });
+
+                    testExpressApp.set('handler override', function(req, res) {
+                        expect(req.method).toBe('GET');
+                        expect(req.url).toBe('/v0/app123/Table?offset=offsetABC');
+                        res.json({
+                            records: [
+                                {
+                                    id: 'recordB',
+                                    fields: {Name: 'Clinton'},
+                                    createdTime: '2020-04-20T16:20:00.000Z',
+                                },
+                            ],
+                        });
+                    });
+                } else if (iterationCounter === 1) {
+                    expect(records.length).toBe(1);
+                    records.forEach(function(record) {
+                        expect(record.id).toBe('recordB');
+                        expect(record.get('Name')).toBe('Clinton');
+                    });
+                }
+                iterationCounter++;
+                fetchNextPage();
+            })
+            .then(function() {
+                expect(iterationCounter).toBe(2);
+                done();
+            });
+    });
+
+    it('selects records and paginates the selection with a done function', function(done) {
+        var iterationCounter = 0;
+
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .eachPage(
+                function page(records, fetchNextPage) {
+                    if (iterationCounter === 0) {
+                        expect(records.length).toBe(1);
+                        records.forEach(function(record) {
+                            expect(record.id).toBe('recordA');
+                            expect(record.get('Name')).toBe('Rebecca');
+                        });
+
+                        testExpressApp.set('handler override', function(req, res) {
+                            expect(req.method).toBe('GET');
+                            expect(req.url).toBe('/v0/app123/Table?offset=offsetABC');
+                            res.json({
+                                records: [
+                                    {
+                                        id: 'recordB',
+                                        fields: {Name: 'Clinton'},
+                                        createdTime: '2020-04-20T16:20:00.000Z',
+                                    },
+                                ],
+                            });
+                        });
+                    } else if (iterationCounter === 1) {
+                        expect(records.length).toBe(1);
+                        records.forEach(function(record) {
+                            expect(record.id).toBe('recordB');
+                            expect(record.get('Name')).toBe('Clinton');
+                        });
+                    }
+                    iterationCounter++;
+                    fetchNextPage();
+                },
+                function(err) {
+                    expect(err).toBeNull();
+                    expect(iterationCounter).toBe(2);
+                    done();
+                }
+            );
+    });
+
     it('selects records with valid params', function(done) {
         testExpressApp.set('handler override', function(req, res) {
             expect(req.method).toBe('GET');
             expect(req.url).toBe(
-                '/v0/app123/Table?maxRecords=50&sort%5B0%5D%5Bfield%5D=Name&sort%5B0%5D%5Bdirection%5D=desc'
+                '/v0/app123/Table?maxRecords=50&sort%5B0%5D%5Bfield%5D=Name&sort%5B0%5D%5Bdirection%5D=desc&cellFormat=json'
             );
             res.json({
                 records: [
@@ -70,7 +319,11 @@ describe('record selection', function() {
         return airtable
             .base('app123')
             .table('Table')
-            .select({maxRecords: 50, sort: [{field: 'Name', direction: 'desc'}]})
+            .select({
+                maxRecords: 50,
+                sort: [{field: 'Name', direction: 'desc'}],
+                cellFormat: 'json',
+            })
             .eachPage(function page(records) {
                 records.forEach(function(record) {
                     expect(record.id).toBe('recordA');
@@ -118,6 +371,78 @@ describe('record selection', function() {
         }).toThrow(/`maxRecords` should be a number/);
     });
 
+    it('eachRecords errors on the first invalid parameter', function() {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return expect(
+            airtable
+                .base('app123')
+                .table('Table')
+                .select()
+                .eachPage('monkeys')
+        ).rejects.toThrow(/must be a function/);
+    });
+
+    it('eachRecords errors on the second invalid parameter', function() {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return expect(
+            airtable
+                .base('app123')
+                .table('Table')
+                .select()
+                .eachPage(function() {}, 'monkeys')
+        ).rejects.toThrow(/must be a function/);
+    });
+
+    it('can throw an error if update fails', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            res.status(402).json({
+                error: {message: 'foo bar'},
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .eachPage(
+                function() {},
+                function(err, result) {
+                    expect(err.statusCode).toBe(402);
+                    expect(err.message).toBe('foo bar');
+                    expect(result).toBeNull();
+                    done();
+                }
+            );
+    });
+
     it('selects records errors when the params are not a plain object', function() {
         return expect(() => {
             airtable
@@ -125,5 +450,80 @@ describe('record selection', function() {
                 .table('Table')
                 .select('?invalid=params');
         }).toThrow(/should be a plain object/);
+    });
+
+    it('selects records the first page of records', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .firstPage(function(err, records) {
+                expect(err).toBeNull();
+                expect(records.length).toBe(1);
+                records.forEach(function(record) {
+                    expect(record.id).toBe('recordA');
+                    expect(record.get('Name')).toBe('Rebecca');
+                });
+                done();
+            });
+    });
+
+    it('firstPage errors without a done function', function() {
+        testExpressApp.set('handler override', function(req, res) {
+            expect(req.method).toBe('GET');
+            expect(req.url).toBe('/v0/app123/Table?');
+            res.json({
+                records: [
+                    {
+                        id: 'recordA',
+                        fields: {Name: 'Rebecca'},
+                        createdTime: '2020-04-20T16:20:00.000Z',
+                    },
+                ],
+                offset: 'offsetABC',
+            });
+        });
+
+        return expect(
+            airtable
+                .base('app123')
+                .table('Table')
+                .select()
+                .firstPage('monkeys')
+        ).rejects.toThrow(/must be a function/);
+    });
+
+    it('firstPage will handle errors', function(done) {
+        testExpressApp.set('handler override', function(req, res) {
+            res.status(402).json({
+                error: {message: 'foo bar'},
+            });
+        });
+
+        return airtable
+            .base('app123')
+            .table('Table')
+            .select()
+            .firstPage(function(err, result) {
+                expect(err.statusCode).toBe(402);
+                expect(err.message).toBe('foo bar');
+                expect(result).toBeNull();
+                done();
+            });
     });
 });
