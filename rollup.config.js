33165530eeadba13dd17b7331e758c86057a68ea
Takeshi Iwana <tak@prismify.co>
2020-07-28 00:59:09 -0600

    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,29 @@
-import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
 import pkg from './package.json'
-import { terser } from 'rollup-plugin-terser'
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const name = 'airtable'
+
 export default {
-  input: 'src/index.ts', // our source file
+  input: './src/index.ts',
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
+  external: [],
+
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+    json(),
+    // Compile TypeScript/JavaScript files
+    babel({ extensions, include: ['src/**/*'] }),
+  ],
+
   output: [
     {
       file: pkg.main,
@@ -10,19 +31,15 @@ export default {
     },
     {
       file: pkg.module,
-      format: 'es', // the preferred format
+      format: 'es',
     },
     {
       file: pkg.browser,
       format: 'iife',
-      name: 'Airtable', // the global which can be used in a browser
+      name,
+
+      // https://rollupjs.org/guide/en#output-globals-g-globals
+      globals: {},
     },
-  ],
-  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-    terser(), // minifies generated bundles
   ],
 }
