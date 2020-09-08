e226d6fb0bf22230da106a95bab0a65de96168d5
Devonte <devo.tox.89@gmail.com>
2020-09-08 02:44:34 +0100

    Update abort-controller.ts

--- src/abort-controller.ts
+++ src/abort-controller.ts
@@ -2,13 +2,11 @@
 let AbortController: new () => AbortController;
 if (typeof window === 'undefined') {
     AbortController = require('abort-controller');
+} else if (typeof window.Request !== 'undefined' && 'signal' in new window.Request('')) {
+    AbortController = window.AbortController;
 } else {
-    if ('signal' in new Request('')) {
-        AbortController = window.AbortController;
-    } else {
-        const polyfill = require('abortcontroller-polyfill/dist/cjs-ponyfill');
-        AbortController = polyfill.AbortController;
-    }
+    const polyfill = require('abortcontroller-polyfill/dist/cjs-ponyfill');
+    AbortController = polyfill.AbortController;
 }
 
 export = AbortController;
