d4f26196d0bf6a03ca3aa7d85939a84a24ef3423
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-09-30 11:32:16 -0400

    fixup! fix github lint warnings

--- src/airtable_base.ts
+++ src/airtable_base.ts
@@ -1,10 +1,9 @@
 import Table from './table';
-import {Base, BaseRequestOptions, BaseResponse} from './base';
+import Base from './base';
 
 export type AirtableBase = {
     (tableName: string): Table;
-    _base: Base;
-    getId(): string;
-    makeRequest(options: BaseRequestOptions): Promise<BaseResponse>;
-    table(tableName: string): Table;
+    getId: Base["getId"];
+    makeRequest: Base["makeRequest"];
+    table: Base["table"];
 };
