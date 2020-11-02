c86672f9e9a6aea143e381076534058454e2aa83
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-11-02 18:17:04 -0500

    Bind fetch to window (#235)
    Solution to #230
    
    Co-authored-by: Alex Rubner <alex.rubner@gmail.com>

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,5 +2,5 @@ import nodeFetch from 'node-fetch';
 
 export = (
     // istanbul ignore next
-    typeof window === 'undefined' ? (nodeFetch as typeof fetch) : fetch
+    typeof window === 'undefined' ? (nodeFetch as typeof fetch) : window.fetch.bind(window)
 );
