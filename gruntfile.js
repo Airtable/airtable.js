3043b7dfc8f64650d846ab5dbdba23f3eaca80db
Kevin Wilde <kevin.wilde@airtable.com>
2022-03-29 07:45:48 -0700

    use umd build for client side bundling

--- gruntfile.js
+++ gruntfile.js
@@ -6,8 +6,29 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: pkg,
         browserify: {
-            client: {
-                src: './lib/airtable.js',
+            umd: {
+                src: './lib/tmp_airtable.js',
+                dest: './lib/airtable.umd.js',
+                options: {
+                    transform: [
+                        [
+                            'envify',
+                            {
+                                _: 'purge',
+                                npm_package_version: pkg.version,
+                            },
+                        ],
+                    ],
+                    preBundleCB: function(b) {
+                        b.require('./lib/tmp_airtable.js', {expose: 'airtable'});
+                    },
+                    browserifyOptions: {
+                        standalone: 'Airtable',
+                    },
+                },
+            },
+            browser: {
+                src: './lib/tmp_airtable.js',
                 dest: './build/airtable.browser.js',
                 options: {
                     transform: [
@@ -20,7 +41,7 @@ module.exports = function(grunt) {
                         ],
                     ],
                     preBundleCB: function(b) {
-                        b.require('./lib/airtable.js', {expose: 'airtable'});
+                        b.require('./lib/tmp_airtable.js', {expose: 'airtable'});
                     },
                 },
             },
