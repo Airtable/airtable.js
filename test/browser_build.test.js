d16a98fb54ad2e16af0e731e4ac983471d2ffbbb
Kevin Wilde <kevin.wilde@airtable.com>
2022-03-29 16:12:35 -0700

    update test

--- test/browser_build.test.js
+++ test/browser_build.test.js
@@ -3,25 +3,30 @@
 var fs = require('fs');
 var path = require('path');
 
-var BUILD_PATH = path.join(__dirname, '..', 'build', 'airtable.browser.js');
+var BUILD_PATHS = [
+    path.join(__dirname, '..', 'build', 'airtable.browser.js'),
+    path.join(__dirname, '..', 'lib', 'airtable.umd.js'),
+];
 
-describe('browser build', function() {
-    it("doesn't contain any environment variables", function(done) {
-        fs.readFile(BUILD_PATH, 'utf8', function(err, builtJs) {
-            if (err) {
-                done(err);
-                return;
-            }
+describe('browser builds', function() {
+    for (const buildPath of BUILD_PATHS) {
+        it("doesn't contain any environment variables", function(done) {
+            fs.readFile(buildPath, 'utf8', function(err, builtJs) {
+                if (err) {
+                    done(err);
+                    return;
+                }
 
-            var exceptions = [/process\.env\.NODE_DEBUG/, /process\.env = {}/];
+                var exceptions = [/process\.env\.NODE_DEBUG/, /process\.env = {}/];
 
-            var builtWithoutExceptions = exceptions.reduce(function(result, exception) {
-                return result.replace(exception, '');
-            }, builtJs);
+                var builtWithoutExceptions = exceptions.reduce(function(result, exception) {
+                    return result.replace(exception, '');
+                }, builtJs);
 
-            expect(builtWithoutExceptions).not.toContain('process.env');
+                expect(builtWithoutExceptions).not.toContain('process.env');
 
-            done();
+                done();
+            });
         });
-    });
+    }
 });
