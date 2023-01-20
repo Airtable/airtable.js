55b8fd9e7e056052f351c7d32af994b2c1f9e352
Will Bowditch <will@bowdi.dev>
2023-01-20 17:25:47 +0000

    adding in new field types, should resolve #272 + #343

--- src/field_set.ts
+++ src/field_set.ts
@@ -1,14 +1,20 @@
 import {Collaborator} from './collaborator';
 import {Attachment} from './attachment';
+import {Barcode} from './barcode';
+import {Button} from './button';
 
 export interface FieldSet {
     [key: string]:
         | undefined
+        | null
         | string
         | number
         | boolean
         | Collaborator
+        | Barcode
+        | Button
         | ReadonlyArray<Collaborator>
         | ReadonlyArray<string>
+        | ReadonlyArray<number | string | boolean | any>
         | ReadonlyArray<Attachment>;
 }
