0432a6110e4c9abd300d7d113fd89eb6ecf914a6
Stephen Heindel <stephen.heindel@gmail.com>
2023-10-20 10:10:52 -0400

    Fix FieldValue for lookup field

--- src/field_set.ts
+++ src/field_set.ts
@@ -1,14 +1,7 @@
 import {Collaborator} from './collaborator';
 import {Attachment} from './attachment';
 
+export type FieldValue = string | number | boolean | Collaborator | ReadonlyArray<Collaborator> | ReadonlyArray<string> | ReadonlyArray<Attachment> | ReadonlyArray<FieldValue>;
 export interface FieldSet {
-    [key: string]:
-        | undefined
-        | string
-        | number
-        | boolean
-        | Collaborator
-        | ReadonlyArray<Collaborator>
-        | ReadonlyArray<string>
-        | ReadonlyArray<Attachment>;
+    [key: string]: FieldValue;
 }
