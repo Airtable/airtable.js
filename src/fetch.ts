b3befd728ff084fb3bb980fa0951be1303373ba3
Sharad K <sharad.k@outlook.com>
2024-01-09 09:23:51 +0000

    Updated dependencies and potential fix

--- src/fetch.ts
+++ src/fetch.ts
@@ -4,4 +4,4 @@ import nodeFetch from 'node-fetch';
 const browserGlobal =
     typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : null; // self is the global in web workers
 
-export = !browserGlobal ? (nodeFetch as typeof fetch) : browserGlobal.fetch.bind(browserGlobal);
+export = !browserGlobal ? nodeFetch : browserGlobal.fetch.bind(browserGlobal);
